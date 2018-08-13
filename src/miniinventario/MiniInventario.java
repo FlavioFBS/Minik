b2f8743fa6490610be2aaa3717e4ce25af7d6b6b
FlavioFBS <flavio-bs95@hotmail.com>
2018-08-13 14:21:47 -0500

    Modificación de prueba

--- src/miniinventario/MiniInventario.java
+++ src/miniinventario/MiniInventario.java
@@ -16,6 +16,7 @@ public class MiniInventario {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Codigo modificado de prueba :v");
     }
     
 }
