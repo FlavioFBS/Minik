8f7afe2586b2c16e3b398e6ca6d13bf8dd606d0a
FlavioFBS <flavio-bs95@hotmail.com>
2018-08-18 00:21:43 -0500

    Version 0.5.2 Cambio de interfaz gráfica para posteriores modificaciones en el backend

--- src/miniinventario/MiniInventario.java
+++ src/miniinventario/MiniInventario.java
@@ -14,9 +14,6 @@ public class MiniInventario {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Codigo modificado de prueba :v");
-    }
+  
     
 }
